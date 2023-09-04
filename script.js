5635dfee992df31b01c86ca5c6fe946b67d2954d
Priyadharshini0411 <142867018+Kartheepan-Devaraj@users.noreply.github.com>
2023-09-04 10:34:16 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// window.addEventListener("scroll", function () {
+//   const one = document.getElementById("one");
+//   const navbar = document.getElementById("navbar");
+//   const navlist = document.getElementById("navl");
+
+//   if (window.scrollY >= 0) {
+//     one.style.backgroundColor = "#000";
+//     navlist.style.Color = "#ffffff";
+//   } else {
+//     one.style.backgroundColor = "#transparent";
+//   }
+// });
